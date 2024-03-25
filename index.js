836e33efefab93204974a8d3e2609cdacae22ca6
Nwankwo Chibueze <nbarnestine1@gmail.com>
2024-03-25 14:16:26 +0100

    second commit

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-// let shareIcon = document.querySelector('#share_icon')
-// let flyInModal = document.querySelector('#link_copied_div')
 
-
-//   shareIcon.addEventListener('click', function(event){
-//     event.preventDefault();
-//     flyInModal.classList.remove('link_copied_div');
-//     flyInModal.classList.add('link_copied_div_new');
-
-//     setTimeout(function() {
-//     flyInModal.classList.remove('link_copied_div_new');
-//     flyInModal.classList.add('link_copied_div');
-//     }, 2000);
-//   })
   let shareIcons = document.querySelectorAll('#share_icon');
   let flyInModal = document.querySelector('#link_copied_div');
   
@@ -21,10 +8,6 @@
       const getLink = event.target.parentElement.getAttribute('href');
       const copyLinkToClipboard = navigator.clipboard.writeText(getLink)
       alert('Link copied to clipboard')
-      // setTimeout(function() {
-      // flyInModal.classList.remove('link_copied_div_new');
-      // flyInModal.classList.add('link_copied_div');
-      // }, 2000);
     });
   });
   
